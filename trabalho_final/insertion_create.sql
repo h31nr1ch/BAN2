7d12b065a2a4b930f4918719db8b3d74c1afe6cd
h31nr1ch <tiagoheinrich1995@gmail.com>
2015-10-25 21:49:37 +0000

    full insert

--- trabalho_final/insertion_create.sql
+++ trabalho_final/insertion_create.sql
@@ -68,8 +68,12 @@ insert into sindicatos values (107,'Christopher');
 insert into sindicatos values (108,'Damon');
 insert into sindicatos values (109,'Harriet');
 
-insert into testes values (5,'Christopher',30);
-insert into testes values (6,'Lori',35);
+insert into testes values (1,'Christopher',30);
+insert into testes values (2,'Lori',35);
+insert into testes values (3,'Ed Marshall',45);
+insert into testes values (4,'Gregory',55);
+insert into testes values (5,'Maria',65);
+insert into testes values (6,'Irving',75);
 insert into testes values (7,'Ed Marshall',45);
 insert into testes values (8,'Gregory',55);
 insert into testes values (9,'Maria',65);
@@ -97,4 +101,58 @@ insert into funcionarios values (8,'Nunez');
 insert into funcionarios values (9,'Beck');
 insert into funcionarios values (10,'Medina');
 
+insert into tecnicos values (1,'Rua Ramon',81069965,1500);
+insert into tecnicos values (2,'Rua Albert',55343416,2500);
+insert into tecnicos values (3,'Rua Molly',11772294,3500);
+insert into tecnicos values (4,'Rua Shari',35540971,4500);
+insert into tecnicos values (5,'Rua James',59764215,1670);
+insert into tecnicos values (6,'Rua Abraham',15830783,6500);
+insert into tecnicos values (7,'Rua Alfredo',41637768,8500);
+insert into tecnicos values (8,'Rua Nick',68665354,9500);
+insert into tecnicos values (9,'Rua Ignacio',10737611,5500);
+insert into tecnicos values (10,'Rua Wesley',16621980,1300);
+
+insert into controladores values (1,'10082015');
+insert into controladores values (2,'20092015');
+insert into controladores values (3,'21052015');
+insert into controladores values (4,'22052015');
+insert into controladores values (5,'20062015');
+insert into controladores values (6,'27052015');
+insert into controladores values (7,'20052015');
+insert into controladores values (8,'30052015');
+insert into controladores values (9,'26052015');
+insert into controladores values (10,'20082015');
+
+insert into manutencao values (1,1,1,'10082015',2,10);
+insert into manutencao values (2,2,2,'10082015',2,10);
+insert into manutencao values (3,3,3,'10082015',2,10);
+insert into manutencao values (4,4,4,'10082015',2,10);
+insert into manutencao values (5,5,5,'10082015',2,10);
+insert into manutencao values (6,6,6,'10082015',2,10);
+insert into manutencao values (7,7,7,'10082015',2,10);
+insert into manutencao values (8,8,8,'10082015',2,10);
+insert into manutencao values (9,9,9,'10082015',2,10);
+insert into manutencao values (10,10,10,'10082015',2,10);
+
+insert into afiliacao values (1,1,105);
+insert into afiliacao values (2,12,4);
+insert into afiliacao values (3,13,106);
+insert into afiliacao values (4,14,107);
+insert into afiliacao values (5,15,108);
+insert into afiliacao values (6,16,105);
+insert into afiliacao values (7,17,105);
+insert into afiliacao values (8,18,109);
+insert into afiliacao values (9,19,109);
+insert into afiliacao values (10,21,4);
+
+insert into pericia values (1,2);
+insert into pericia values (2,3);
+insert into pericia values (3,4);
+insert into pericia values (4,5);
+insert into pericia values (5,6);
+insert into pericia values (6,7);
+insert into pericia values (7,8);
+insert into pericia values (8,9);
+insert into pericia values (9,10);
+insert into pericia values (10,1);
 
