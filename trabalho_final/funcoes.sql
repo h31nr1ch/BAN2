470aad01549074fa84a78ad3e9ca1a903472c1a8
h31nr1ch <tiagoheinrich1995@gmail.com>
2015-10-28 22:19:33 +0000

    sdas

--- trabalho_final/funcoes.sql
+++ trabalho_final/funcoes.sql
@@ -1,11 +1,13 @@
 CREATE FUNCTION funcionariosApos(funcionario varchar (50))
 RETURNS varchar (50)
-RETURN (SELECT *
-        FROM  testes
-        WHERE pont_max >= 8)
+RETURN (SELECT * FROM  testes WHERE pont_max >= 8)
+
+CREATE FUNCTION funcionariosexames(funcionario varchar (50))
+RETURNS varchar (50)
+RETURN (SELECT * FROM  testes WHERE pont_max < 5)
 
 DELIMITER $$
-CREATE TRIGGER DELLpesoNeg
+CREATE TRIGGER capacidadesss
 BEFORE UPDATE ON modelos
 FOR EACH ROW BEGIN
 delete from modelos where modelos.capacidade < 0;
@@ -27,4 +29,13 @@ FOR EACH ROW BEGIN
 delete from tecnicos where tecnicos.pont_max < 0;
  END$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER horaspositivas
+BEFORE UPDATE ON manutencao
+FOR EACH ROW BEGIN
+delete from manutencao where manutencao.horas_gastas < 0;
+ END$$
+DELIMITER ;
+
 
