e95ffcc6577f4e8aba6605956988f3f12d6d5b22
h31nr1ch <tiagoheinrich1995@gmail.com>
2015-11-22 22:05:02 +0000

    sda

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Banco de dados 2
 
 # utilizar para rodar o TerraER 
 java -jar TerraER2.12.jar
+
+#
+https://rayed.com/wordpress/?p=1266
+http://www.programeempython.com.br/blog/bancos-de-dados-com-python-sqlite3/
+http://pythonclub.com.br/gerenciando-banco-dados-sqlite3-python-parte1.html
+https://pythonhelp.wordpress.com/tag/banco-de-dados/
--- python/ban.py
+++ python/ban.py
@@ -1,1 +1,35 @@
-import 
+#CRUD python
+#uhulll
+#!/usr/bin/env python
+ 
+import MySQLdb
+ 
+# Instantiate connection object and connect to MySQL database
+db = MySQLdb.connect(&quot;localhost&quot;, &quot;username&quot;, &quot;password&quot;, &quot;irent&quot;)
+ 
+# Instantiate cursor object
+cursor = db.cursor()
+ 
+# Prepare a dml statement
+sql = &quot;insert into property (address_line_1, user_id, type_id) \
+        values ('%s', '%d', '%d')&quot; % ('234 Sathorn Rd', 1, 1)
+ 
+try:
+    # Execute dml and commit changes
+    cursor.execute(sql)
+    db.commit()
+     
+except:
+    # Rollback changes
+    db.rollback()
+ 
+# Close database connection
+db.close()
+
+# Prepare a dml statement
+sql = &quot;delete from property where id &gt; '%d'&quot; % (10)
+
+# Prepare a dml statement
+sql = &quot;update property set address_line_2 = '%s' \
+        where id = '%d'&quot; % ('Apt B', 1)
+ 
